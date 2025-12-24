7ea8459650be7d8e690103e444ba575f75084528
박건우 <parkwoo9901@gmail.com>
2025-12-24 17:54:22 +0900

    feat: Add Elasticsearch Config

---
+++ ragcraft/src/main/java/lab/context/ragcraft/elasticsearch/ElasticsearchConfig.java
@@ -0,0 +1,26 @@
+package lab.context.ragcraft.elasticsearch;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchConfig {
+
+    @Value("${elasticsearch.host}")
+    private String esHost;
+
+    @Value("${elasticsearch.apiKey}")
+    private String esApiKey;
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+
+        return ElasticsearchClient.of(b -> b
+                .host(esHost)
+                .apiKey(esApiKey)
+        );
+    }
+
+}
